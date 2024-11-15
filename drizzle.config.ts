0c235b0e32403bd0000583d6f2f7286ee24ec595
Chris Cardoza <chris.siya511@gmail.com>
2024-11-15 11:11:40 -0800

    drizzle orm and biome linter

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({
+  path: ".env.local",
+});
+
+export default defineConfig({
+  schema: "./lib/db/schema.ts",
+  out: "./lib/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    url: process.env.POSTGRES_URL!,
+  },
+});
